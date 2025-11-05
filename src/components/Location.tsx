74deab0782b002cb96acc595670d436f029c9afc
openVietAnh <vietanhtran.uet@gmail.com>
2025-11-05 18:23:30 +0700

    fix map position

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -13,7 +13,7 @@ const Location = () => {
           {t("location.description")}
         </p>
 
-        <div className="w-full h-96 md:h-[500px] rounded-lg overflow-hidden shadow-lg">
+        <div className="flex justify-center w-full h-96 md:h-[500px] rounded-lg overflow-hidden shadow-lg">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.9593112478374!2d105.8149241!3d21.034314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab97265bd46f%3A0x636380a6dae63a71!2sWins%20Residences%20Hanoi!5e0!3m2!1svi!2s!4v1762341338342!5m2!1svi!2s"
             width="600"
